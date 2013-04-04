2de90bbc347a5ba5666e9598b375853bb98858c5
Jai Hirsch <jai.hirsch@gmail.com>
2013-04-03 21:15:11 -0500

    Update init_sharded_env.bat

--- mongodb-java-examples/init_sharded_env.bat
+++ mongodb-java-examples/init_sharded_env.bat
@@ -1,12 +1,10 @@
+REM  Andrew Erlichson - Original author
+REM  Jai Hirsch       - Translated original .sh file to .bat   
+REM  10gen
+REM  script to start a sharded environment on localhost
 
-ECHO  Andrew Erlichson - Original .sh file
-ECHO  Jai Hirsch - .bat file translation
-ECHO  10gen
-ECHO  script to start a sharded environment on localhost
+echo "del data files for a clean start"
 
-ECHO  clean everything up
-
-echo "Cleaning old data files"
 del /Q c:\data\config
 del /Q c:\data\shard0
 del /Q c:\data\shard1
@@ -15,8 +13,8 @@ del /Q c:\data\config
 
 PING 1.1.1.1 -n 1 -w 5000 >NUL
 
+REM  start a replica set and tell it that it will be a shard0
 
-ECHO  start a replica set and tell it that it will be a shord0
 mkdir c:\data\shard0\rs0
 mkdir c:\data\shard0\rs1
 mkdir c:\data\shard0\rs2
@@ -26,15 +24,13 @@ start mongod --replSet s0 --dbpath c:\data\shard0\rs2 --port 37019  --shardsvr -
 
 PING 1.1.1.1 -n 1 -w 5000 >NUL
 
-ECHO  connect to one server and initiate the set
+REM  connect to one server and initiate the set
 start mongo --port 37017 --eval "config = { _id: 's0', members:[{ _id : 0, host : 'localhost:37017' },{ _id : 1, host : 'localhost:37018' },{ _id : 2, host : 'localhost:37019' }]};rs.initiate(config)"
 
-
-ECHO  start a replicate set and tell it that it will be a shard1
+REM  start a replicate set and tell it that it will be a shard1
 mkdir c:\data\shard1\rs0
 mkdir c:\data\shard1\rs1
 mkdir c:\data\shard1\rs2
-
 
 start mongod --replSet s1 --dbpath c:\data\shard1\rs0 --port 47017  --shardsvr --smallfiles --oplogSize 100
 start mongod --replSet s1 --dbpath c:\data\shard1\rs1 --port 47018  --shardsvr --smallfiles --oplogSize 100
@@ -44,8 +40,7 @@ PING 1.1.1.1 -n 1 -w 5000 >NUL
 
 start mongo --port 47017 --eval "config = { _id: 's1', members:[{ _id : 0, host : 'localhost:47017' },{ _id : 1, host : 'localhost:47018' },{ _id : 2, host : 'localhost:47019' }]};rs.initiate(config);"
 
-
-ECHO  start a replicate set and tell it that it will be a shard2
+REM  start a replicate set and tell it that it will be a shard2
 mkdir c:\data\shard2\rs0
 mkdir c:\data\shard2\rs1
 mkdir c:\data\shard2\rs2
@@ -57,9 +52,7 @@ PING 1.1.1.1 -n 1 -w 5000 >NUL
 
 start mongo --port 57017 --eval "config = { _id: 's2', members:[{ _id : 0, host : 'localhost:57017' },{ _id : 1, host : 'localhost:57018' },{ _id : 2, host : 'localhost:57019' }]};rs.initiate(config)"
 
-
-
-ECHO  now start 3 config servers
+REM  now start 3 config servers
 
 mkdir c:\data\config\config-a
 mkdir c:\data\config\config-b
@@ -76,10 +69,8 @@ start mongos  --configdb localhost:57040,localhost:57041,localhost:57042
 
 echo "Wait 60 seconds for the replica sets to fully come online"
 PING 1.1.1.1 -n 1 -w 60000 >NUL
+
 echo "Connnecting to mongos and enabling sharding"
 
-ECHO  add shards and enable sharding on the test db
+REM  add shards and enable sharding on the test db
 start  mongo  --eval "db.adminCommand( { addshard : 's0/'+'localhost:37017' } );db.adminCommand( { addshard : 's1/'+'localhost:47017' } );db.adminCommand( { addshard : 's2/'+'localhost:57017' } );db.adminCommand({enableSharding: 'test'});db.adminCommand({shardCollection: 'test.grades', key: {student_id:1}});"
-
-
-
