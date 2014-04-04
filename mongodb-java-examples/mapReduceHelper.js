a182d7a9ce12b9fa02f6dcc5648e536c86c1500d
Jai Hirsch <jai.hirsch@gmail.com>
2014-04-04 09:16:31 -0500

    Update mapReduceHelper.js

--- mongodb-java-examples/mapReduceHelper.js
+++ mongodb-java-examples/mapReduceHelper.js
@@ -1,6 +1,29 @@
 /*
    Author:  David Abadir
    Use:  mongo <mongohost>:<port> [options] --shell  "path\to\script\mapReduceHelper.js"
+   
+   Example output:
+   
+   mongos> progressMapReduce()                                                                                                  
+   {                                                                                                                            
+        "percentDone" : "77.59",                                                                                             
+        "estimated_seconds_remaining" : "216683.85",                                                                         
+        "estimated_complete_date" : "Sun Mar 30 2014 01:26:06 GMT-0500 (Central Daylight Time)",                             
+        "msg" : "m/r: (1/3) emit phase M/R: (1/3) Emit Progress: 118530099/152770251 77%"                                    
+   }                                                                                                                            
+   {                                                                                                                            
+        "percentDone" : "61.95",                                                                                             
+        "estimated_seconds_remaining" : "265751.41",                                                                         
+        "estimated_complete_date" : "Sun Mar 30 2014 15:03:54 GMT-0500 (Central Daylight Time)",                             
+        "msg" : "m/r: (1/3) emit phase M/R: (1/3) Emit Progress: 94640299/152774026 61%"                                     
+   }                                                                                                                            
+   {                                                                                                                            
+        "percentDone" : "35.10",                                                                                             
+        "estimated_seconds_remaining" : "295193.73",                                                                         
+        "estimated_complete_date" : "Sun Mar 30 2014 23:14:36 GMT-0500 (Central Daylight Time)",                             
+        "msg" : "m/r: (1/3) emit phase M/R: (1/3) Emit Progress: 53645199/152849504 35%"                                     
+  }                                                                                                                            
+
 */
 var progressMapReduce=function(){
    db.currentOp().inprog.forEach(
