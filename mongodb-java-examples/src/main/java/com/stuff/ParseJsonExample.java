45c2b6172e1e104c050469028522631dc7ac5fc0
Jai Hirsch <jai.hirsch@gmail.com>
2015-01-20 18:05:33 -0600

    cleaned

--- mongodb-java-examples/src/main/java/com/stuff/ParseJsonExample.java
+++ mongodb-java-examples/src/main/java/com/stuff/ParseJsonExample.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.mongodb.AggregationOutput;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.util.JSON;
@@ -18,6 +19,17 @@ public class ParseJsonExample {
             mc = new MongoClient();
             DBCollection col = mc.getDB("students").getCollection("grades");
 
+            // simple DBObject parse:
+
+            Object simpleParse = JSON.parse("{type:'exam',score:{$gt:65, $lt:75}}");
+
+            DBCursor simpleFind = col.find((DBObject) simpleParse).limit(5);
+            while (simpleFind.hasNext()) {
+                System.out.println(simpleFind.next());
+            }
+            simpleFind.close();
+
+            // complex DBList parse:
             Object parse = JSON
                     .parse("[{'$group':{'_id':'$student_id', 'average':{$avg:'$score'}}}, {'$sort':{'average':-1}}, {'$limit':1}]");
             if (parse instanceof List<?>) {
@@ -25,6 +37,9 @@ public class ParseJsonExample {
 
                 AggregationOutput aggregate = col.aggregate(query);
                 System.out.println(aggregate.results().iterator().next());
+            } else {
+                System.out.println("Unexpected result from JSON parse, expected List<?> but was "
+                        + parse.getClass().getName());
             }
         } finally {
             if (null != mc)
